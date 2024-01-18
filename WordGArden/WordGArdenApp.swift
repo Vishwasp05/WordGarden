32b0d78f38f1622534cbe72ee9a74ab6f9c38dd4
Vishwas Sharma <ch0nky@protonmail.com>
2024-01-18 18:15:32 +0530

    Initial Commit

---
+++ WordGArden/WordGArdenApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordGArdenApp.swift
+//  WordGArden
+//
+//  Created by Vishwas Sharma on 18/01/24.
+//
+
+import SwiftUI
+
+@main
+struct WordGArdenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
